879a87dad6d48bd1d5d9167b9f85b79653978bc9
yangjourney <yangbo145@hotmail.com>
2023-04-12 17:33:19 +0800

    Update worldcup.sql

--- worldcup.sql
+++ worldcup.sql
@@ -1,1 +1,14 @@
+CREATE TABLE teams(
+team_id SERIAL PRIMARY KEY NOT NULL,
+name varchar(60) not null  UNIQUE);
 
+CREATE TABLE games(
+game_id SERIAL PRIMARY KEY NOT NULL,
+year int NOT NULL,
+round varchar(60) NOT NULL,
+winner_id int NOT NULL,
+opponent_id int NOT NULL,
+winner_goals int NOT NULL,
+opponent_goals int NOT NULL,
+FOREIGN KEY(winner_id) REFERENCES teams(team_id),
+FOREIGN KEY(opponent_id) REFERENCES teams(team_id));
