cfba7340fe13767786e5d867e25fe28a6eebc1a3
yangjourney <yangbo145@hotmail.com>
2023-04-14 16:21:41 +0800

    Update worldcup.sql

--- worldcup.sql
+++ worldcup.sql
@@ -1,14 +1,239 @@
-CREATE TABLE teams(
-team_id SERIAL PRIMARY KEY NOT NULL,
-name varchar(60) not null  UNIQUE);
+--
+-- PostgreSQL database dump
+--
 
-CREATE TABLE games(
-game_id SERIAL PRIMARY KEY NOT NULL,
-year int NOT NULL,
-round varchar(60) NOT NULL,
-winner_id int NOT NULL,
-opponent_id int NOT NULL,
-winner_goals int NOT NULL,
-opponent_goals int NOT NULL,
-FOREIGN KEY(winner_id) REFERENCES teams(team_id),
-FOREIGN KEY(opponent_id) REFERENCES teams(team_id));
+-- Dumped from database version 12.9 (Ubuntu 12.9-2.pgdg20.04+1)
+-- Dumped by pg_dump version 12.9 (Ubuntu 12.9-2.pgdg20.04+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: games; Type: TABLE; Schema: public; Owner: freecodecamp
+--
+
+CREATE TABLE public.games (
+    game_id integer NOT NULL,
+    year integer NOT NULL,
+    round character varying(60) NOT NULL,
+    winner_id integer NOT NULL,
+    opponent_id integer NOT NULL,
+    winner_goals integer NOT NULL,
+    opponent_goals integer NOT NULL
+);
+
+
+ALTER TABLE public.games OWNER TO freecodecamp;
+
+--
+-- Name: games_game_id_seq; Type: SEQUENCE; Schema: public; Owner: freecodecamp
+--
+
+CREATE SEQUENCE public.games_game_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.games_game_id_seq OWNER TO freecodecamp;
+
+--
+-- Name: games_game_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: freecodecamp
+--
+
+ALTER SEQUENCE public.games_game_id_seq OWNED BY public.games.game_id;
+
+
+--
+-- Name: teams; Type: TABLE; Schema: public; Owner: freecodecamp
+--
+
+CREATE TABLE public.teams (
+    team_id integer NOT NULL,
+    name character varying(60) NOT NULL
+);
+
+
+ALTER TABLE public.teams OWNER TO freecodecamp;
+
+--
+-- Name: teams_team_id_seq; Type: SEQUENCE; Schema: public; Owner: freecodecamp
+--
+
+CREATE SEQUENCE public.teams_team_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.teams_team_id_seq OWNER TO freecodecamp;
+
+--
+-- Name: teams_team_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: freecodecamp
+--
+
+ALTER SEQUENCE public.teams_team_id_seq OWNED BY public.teams.team_id;
+
+
+--
+-- Name: games game_id; Type: DEFAULT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.games ALTER COLUMN game_id SET DEFAULT nextval('public.games_game_id_seq'::regclass);
+
+
+--
+-- Name: teams team_id; Type: DEFAULT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.teams ALTER COLUMN team_id SET DEFAULT nextval('public.teams_team_id_seq'::regclass);
+
+
+--
+-- Data for Name: games; Type: TABLE DATA; Schema: public; Owner: freecodecamp
+--
+
+COPY public.games (game_id, year, round, winner_id, opponent_id, winner_goals, opponent_goals) FROM stdin;
+33	2018	Final	26	25	4	2
+34	2018	Third Place	28	27	2	0
+35	2018	Semi-Final	25	27	2	1
+36	2018	Semi-Final	26	28	1	0
+37	2018	Quarter-Final	25	29	3	2
+38	2018	Quarter-Final	27	30	2	0
+39	2018	Quarter-Final	28	31	2	1
+40	2018	Quarter-Final	26	32	2	0
+41	2018	Eighth-Final	27	33	2	1
+42	2018	Eighth-Final	30	34	1	0
+43	2018	Eighth-Final	28	35	3	2
+44	2018	Eighth-Final	31	36	2	0
+45	2018	Eighth-Final	25	37	2	1
+46	2018	Eighth-Final	29	38	2	1
+47	2018	Eighth-Final	32	39	2	1
+48	2018	Eighth-Final	26	40	4	3
+49	2014	Final	41	40	1	0
+50	2014	Third Place	42	31	3	0
+51	2014	Semi-Final	40	42	1	0
+52	2014	Semi-Final	41	31	7	1
+53	2014	Quarter-Final	42	43	1	0
+54	2014	Quarter-Final	40	28	1	0
+55	2014	Quarter-Final	31	33	2	1
+56	2014	Quarter-Final	41	26	1	0
+57	2014	Eighth-Final	31	44	2	1
+58	2014	Eighth-Final	33	32	2	0
+59	2014	Eighth-Final	26	45	2	0
+60	2014	Eighth-Final	41	46	2	1
+61	2014	Eighth-Final	42	36	2	1
+62	2014	Eighth-Final	43	47	2	1
+63	2014	Eighth-Final	40	34	1	0
+64	2014	Eighth-Final	28	48	2	1
+\.
+
+
+--
+-- Data for Name: teams; Type: TABLE DATA; Schema: public; Owner: freecodecamp
+--
+
+COPY public.teams (team_id, name) FROM stdin;
+25	Croatia
+26	France
+27	England
+28	Belgium
+29	Russia
+30	Sweden
+31	Brazil
+32	Uruguay
+33	Colombia
+34	Switzerland
+35	Japan
+36	Mexico
+37	Denmark
+38	Spain
+39	Portugal
+40	Argentina
+41	Germany
+42	Netherlands
+43	Costa Rica
+44	Chile
+45	Nigeria
+46	Algeria
+47	Greece
+48	United States
+\.
+
+
+--
+-- Name: games_game_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
+--
+
+SELECT pg_catalog.setval('public.games_game_id_seq', 64, true);
+
+
+--
+-- Name: teams_team_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
+--
+
+SELECT pg_catalog.setval('public.teams_team_id_seq', 48, true);
+
+
+--
+-- Name: games games_pkey; Type: CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.games
+    ADD CONSTRAINT games_pkey PRIMARY KEY (game_id);
+
+
+--
+-- Name: teams teams_name_key; Type: CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.teams
+    ADD CONSTRAINT teams_name_key UNIQUE (name);
+
+
+--
+-- Name: teams teams_pkey; Type: CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.teams
+    ADD CONSTRAINT teams_pkey PRIMARY KEY (team_id);
+
+
+--
+-- Name: games games_opponent_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.games
+    ADD CONSTRAINT games_opponent_id_fkey FOREIGN KEY (opponent_id) REFERENCES public.teams(team_id);
+
+
+--
+-- Name: games games_winner_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.games
+    ADD CONSTRAINT games_winner_id_fkey FOREIGN KEY (winner_id) REFERENCES public.teams(team_id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
